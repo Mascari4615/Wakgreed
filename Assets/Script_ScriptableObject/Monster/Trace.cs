a54b566009538a8b2fe98b083f8a64972c92785c
Ttmdacl <55438621+Ttmdacl@users.noreply.github.com>
2021-09-19 03:56:43 +0900

    마흔 아홉 번째 커밋
    딱히 달라진 건 없음
    
    Linq 공부
    게임 개발 설계 관련 공부 (게임 엔진과 컴포넌트)

---
+++ Assets/Script_ScriptableObject/Monster/Trace.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Trace : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
