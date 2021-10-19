b9fd409e6da2d3bd4ea0e02eae760fccc0fd561c
Mascari4615 <55438621+Ttmdacl@users.noreply.github.com>
2021-10-19 23:57:47 +0900

    80 - UI 개선
    에셋번들 공부

--- Assets/DashUI.cs
+++ Assets/DashUI.cs
@@ -3,14 +3,37 @@ using UnityEngine;
 public class DashUI : MonoBehaviour
 {
     [SerializeField] private IntVariable curDashStack;
-    [SerializeField] private GameObject[] dashStackUIs;
+    [SerializeField] private IntVariable maxDashStack;
+    private Transform[] dashStackUIs;
 
-    public void UpdateUI()
+    private void Awake() 
+    {
+        dashStackUIs = new Transform[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            dashStackUIs[i] = transform.GetChild(i);
+        }
+
+        UpdateCurDashStackUI();
+        UpdateMaxDashStackUI();
+    }
+
+    public void UpdateCurDashStackUI()
+    {
+        for (int i = 0; i < maxDashStack.RuntimeValue; i++)
+        {
+            if (i < curDashStack.RuntimeValue) dashStackUIs[i].GetChild(0).gameObject.SetActive(true);
+            else dashStackUIs[i].GetChild(0).gameObject.SetActive(false);
+        }
+    }
+
+    public void UpdateMaxDashStackUI()
     {
         for (int i = 0; i < dashStackUIs.Length; i++)
         {
-            if (i < curDashStack.RuntimeValue) dashStackUIs[i].transform.GetChild(0).gameObject.SetActive(true);
-            else dashStackUIs[i].transform.GetChild(0).gameObject.SetActive(false);
+            if (i < maxDashStack.RuntimeValue) dashStackUIs[i].gameObject.SetActive(true);
+            else dashStackUIs[i].gameObject.SetActive(false);
         }
     }
 }
