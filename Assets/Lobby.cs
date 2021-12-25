d61af2ffc4e81578eb85ac572b534a4a5d4f043a
Ttmdacl <55438621+Ttmdacl@users.noreply.github.com>
2021-12-25 23:38:14 +0900

    127 - Lobby NPCs

--- Assets/Lobby.cs
+++ Assets/Lobby.cs
@@ -1,41 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Lobby : MonoBehaviour
 {
     [SerializeField] private GameObject banggalTree;
-    [SerializeField] private GameObject angel;
-    [SerializeField] private GameObject secret;
-
     [SerializeField] private GameObject messiBar;
-    [SerializeField] private GameObject rusuk;
-
     [SerializeField] private GameObject dopamineLab;
-    [SerializeField] private GameObject seu;
-    [SerializeField] private GameObject dopamine;
-
     [SerializeField] private GameObject library;
-    [SerializeField] private GameObject leeduksoo;
-    [SerializeField] private GameObject bujung;
+    [SerializeField] private GameObject convenienceStore;
+    [SerializeField] private NPC[] npcs;
+    [SerializeField] CinemachineVirtualCamera virtualCamera;
 
     private void OnEnable()
     {
+        virtualCamera.m_Lens.OrthographicSize = 12;   
         GameData curGameData = DataManager.Instance.CurGameData;
 
         banggalTree.SetActive(curGameData.rescuedNPC[0]);
-        angel.SetActive(curGameData.rescuedNPC[0]);
-        secret.SetActive(curGameData.rescuedNPC[2]);
-
         messiBar.SetActive(curGameData.rescuedNPC[1]);
-        rusuk.SetActive(curGameData.rescuedNPC[1]);
+        dopamineLab.SetActive(curGameData.rescuedNPC[6] || curGameData.rescuedNPC[7] || curGameData.rescuedNPC[11] || curGameData.rescuedNPC[21]);
+        library.SetActive(curGameData.rescuedNPC[8] || curGameData.rescuedNPC[10] || curGameData.rescuedNPC[17] || curGameData.rescuedNPC[18]);
+        convenienceStore.SetActive(curGameData.rescuedNPC[28]);
 
-        dopamineLab.SetActive(curGameData.rescuedNPC[6] || curGameData.rescuedNPC[7]);
-        dopamine.SetActive(curGameData.rescuedNPC[6]);
-        seu.SetActive(curGameData.rescuedNPC[7]);
-
-        library.SetActive(curGameData.rescuedNPC[8] || curGameData.rescuedNPC[10]);
-        leeduksoo.SetActive(curGameData.rescuedNPC[8]);
-        bujung.SetActive(curGameData.rescuedNPC[10]);
+        for (int i = 0; i < npcs.Length; i++)
+            npcs[i].gameObject.SetActive(curGameData.rescuedNPC[i]);
     }
 }
