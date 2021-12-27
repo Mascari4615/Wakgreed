0d83df7cb0970436b3d50637fe44c1663ae5eda7
Ttmdacl <55438621+Ttmdacl@users.noreply.github.com>
2021-12-28 00:17:59 +0900

    156 -asdfgsregrssrgsrgs

--- Assets/Sohpia.cs
+++ Assets/Sohpia.cs
@@ -1,8 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class Sohpia : ShopKeeper
 {
-
+    public override void FocusOff()
+    {
+        base.FocusOff();
+        DataManager.Instance.CurGameData.talkedOnceNPC[ID] = true;
+        DataManager.Instance.CurGameData.rescuedNPC[ID] = true;
+    }
 }
