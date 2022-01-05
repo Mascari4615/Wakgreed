439e84bba9a96563ad133151f064945fe607bdf0
Ttmdacl <55438621+Ttmdacl@users.noreply.github.com>
2022-01-05 20:00:25 +0900

    199 - 0.96 엔젤

--- Assets/Angel.cs
+++ Assets/Angel.cs
@@ -11,31 +11,57 @@ public class Angel : NPC
     [SerializeField] private IntVariable power;
     [SerializeField] private MaxHp maxhp;
     [SerializeField] private TextMeshProUGUI curGrowthText;
-    [SerializeField] private TextMeshProUGUI powerText;
-    [SerializeField] private TextMeshProUGUI hpText;
+    [SerializeField] private Image[] curEffectStamps;
     [SerializeField] private Image treeImage;
     private int bonusPower = 0;
     private int bonusHp = 0;
 
-    private void OnEnable() => UpdateTree();
-
-    private void UpdateTree()
+    protected override void Awake()
+    {
+        base.Awake();
+        UpdateTree();
+    }
+    public void UpdateTree()
     {
         maxhp.RuntimeValue -= bonusHp;
-        power.RuntimeValue -= bonusPower;
-
+        // power.RuntimeValue -= bonusPower;
         bonusHp = 0;
-        bonusPower = 0;
+        // bonusPower = 0;
         curGrowthText.text = $"<color=#C6FF4C>현재 성장치</color> : {DataManager.Instance.CurGameData.deathCount}";
+
+        curEffectStamps[0].gameObject.SetActive(DataManager.Instance.CurGameData.deathCount >= 3);
+        curEffectStamps[1].gameObject.SetActive(DataManager.Instance.CurGameData.deathCount >= 6);
+        curEffectStamps[2].gameObject.SetActive(DataManager.Instance.CurGameData.deathCount >= 9);
+        curEffectStamps[3].gameObject.SetActive(DataManager.Instance.CurGameData.deathCount >= 12);
 
         treeImage.sprite = treeSprites[Mathf.Clamp((DataManager.Instance.CurGameData.deathCount - 1) / 2, 0, 3)];
         treeSpriteRenderer.sprite = treeSprites[Mathf.Clamp((DataManager.Instance.CurGameData.deathCount - 1) / 2, 0, 3)];
         shadowSpriteRenderer.sprite = shadowSprites[Mathf.Clamp((DataManager.Instance.CurGameData.deathCount - 1) / 2, 0, 3)];
 
-        powerText.text = $"- 힘, <color=#C6FF4C>성장치</color> X 2 = {DataManager.Instance.CurGameData.deathCount * 2} 추가";
-        hpText.text = $"- 체력, <color=#C6FF4C>성장치</color> X 1 = {DataManager.Instance.CurGameData.deathCount * 1} 추가";
+        if (DataManager.Instance.CurGameData.deathCount >= 3)
+        {
+            bonusHp += 3;
+        }
 
-        maxhp.RuntimeValue += DataManager.Instance.CurGameData.deathCount * 2;
-        power.RuntimeValue += DataManager.Instance.CurGameData.deathCount * 1;
+        if (DataManager.Instance.CurGameData.deathCount >= 6)
+        {
+            bonusHp += 3;
+            // bonusPower += 3;
+        }
+
+        if (DataManager.Instance.CurGameData.deathCount >= 9)
+        {
+            bonusHp += 3;
+            // bonusPower += 5;
+        }
+
+        if (DataManager.Instance.CurGameData.deathCount >= 12)
+        {
+            bonusHp += 3;
+            // bonusPower += 10;
+        }
+
+        maxhp.RuntimeValue += bonusHp;
+        // power.RuntimeValue += bonusPower;
     }
 }
